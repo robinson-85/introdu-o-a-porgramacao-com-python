e4a0c463ea06c14b0ab4d3fc7c9de9db3bb9dfd8
robinson-85 <robinpaulista@yahoo.com.br>
2020-07-08 16:23:35 -0300

    exercicios2.1.py

---
+++ exercicios2.1.py
@@ -0,0 +1,10 @@
+Python 3.8.3 (default, Jun  6 2020, 12:03:24) 
+[GCC 7.5.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 10+20*30
+610
+>>> 4*4/30
+0.5333333333333333
+>>> 9*9*9*9*6-1
+39365
+>>> 
