1193b24bc47d658257455f572fb5e1b5b935f6b1
robinson-85 <robinpaulista@yahoo.com.br>
2020-07-29 12:48:49 -0300

    capitulo 3 - inclusões

---
+++ exercicios_programas/exercicio3.1.py
@@ -0,0 +1,11 @@
+Complete a tabela a seguir, marcando inteiro ou ponto
+flutuante dependendo do número apresentado.
+
+Número            Tipo numérico
+
+5                 inteiro
+5.0               flutuante
+4.3               flutuante
+-2                inteiro
+100               inteiro
+1.333             flutuante
