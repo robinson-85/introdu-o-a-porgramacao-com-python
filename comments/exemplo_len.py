f2f84acfaf42a514f3ae3e44efa5825c8c069ece
Robinson <robinpaulista@yahoo.com.br>
2020-10-12 08:43:08 -0300

    New comments

---
+++ comments/exemplo_len.py
@@ -0,0 +1,7 @@
+len("A")
+
+len("AB")
+
+len("")
+
+len("O rato roeu a roupa")
---
+++ comments/exercicio3.2.py
@@ -0,0 +1,24 @@
+#Complete a tabela a seguir, rerspondendo True ou False. Considere a = 4, b = 10, 
+# c = 5.0,  d = 1 e f = 5
+
+a == c # False
+
+a < b  # True
+
+d > b # False
+
+c != f # False
+
+a == b # False
+
+c > d  # True
+
+b < a  # False
+
+c >= f  # True
+
+f >= c # True
+
+c <= c  # True
+
+c <= f # False
