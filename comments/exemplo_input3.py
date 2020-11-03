6b2fb6fdc3218b1277033571785d8670d77f3e8d
Robinson <robinpaulista@yahoo.com.br>
2020-11-03 15:34:59 -0300

    Capitulo 3

---
+++ comments/exemplo_input3.py
@@ -0,0 +1,4 @@
+anos = int(input("Anos de serviço: "))
+valor_por_ano = float(input("Valor por ano: "))
+bônus = anos * valor_por_ano
+print(f"Bônus de R$: {bônus:5.2f}")
---
+++ exercicios_programas/exercicio3.15.py
@@ -0,0 +1,4 @@
+'''Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte
+a quantidade de cigarros fumados por dia e quantos anos ela já fumou. Considere que 
+um fumante perde 10 minutos de vida a cada cigarrp e calcule quantos dias de vida um
+fumante perderá. Exiba o total em dias.'''
