8d37e9cec8bcc97a4126f39f2292f9359366d609
Robinson <robinpaulista85@gmail.com>
2020-12-04 17:32:07 -0300

    Novas inserções

--- exercicios_programas/exercicio4.5.py
+++ exercicios_programas/exercicio4.5.py
@@ -1,2 +1,15 @@
 ''' 4.5 Execute o Programa 4.4 e experimente alguns valores. Verifique se os resultados 
 foram os mesmos do Programa 4.2.'''
+
+salário = float(input("Digite o salário: R$"))
+base = salário 
+aumento = 0
+if base > 1250:
+    aumento = salário * 0.1
+    aumento = salário + aumento
+if base <= 1250:
+    aumento = salário * 0.15
+    aumento = salário + aumento
+
+
+print(f"Salário: R${salário:6.2f} Aumento do salário: R${aumento:6.2f}")
--- exercicios_programas/exercicio4.6.py
+++ exercicios_programas/exercicio4.6.py
@@ -1,3 +1,13 @@
 ''' 4.6 Escreva um programa que pergunte a distância que um passageiro deseja percorrer
 em km. Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de até 200 km,
 e R$ 0,45 para viagens mais longas. '''
+
+distância = float(input("Digite a distância que você quer percorrer: "))
+
+if distância > 200:
+    preco_passagem = 0.50 * (distância)
+else:
+    preco_passagem = 0.45 * (distância)
+    
+print(f"A distância é: {distância:6.2f} km \n")
+print(f"O preço da passagem é: R$ {preco_passagem:6.2f} \n")
